c660dbc7f38a30f91fae26a95b2dc9bfe9321a57
developers@sweatworks.net <developers@sweatworks.net>
2025-03-28 18:36:03 -0500

    merge changes and fix chat and locations

--- MallCyberShop/app/home/chat-button.tsx
+++ MallCyberShop/app/home/chat-button.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Text, Pressable } from "react-native";
+import { useRouter } from "expo-router";
 
 const ChatButton = () => {
   const [color, setColor] = useState("#007AFF");
+  const router = useRouter();
 
   // Función para generar un color aleatorio
   const getRandomColor = () => {
@@ -14,20 +16,23 @@ const ChatButton = () => {
     return color;
   };
 
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       setColor(getRandomColor());
-//     }, 500); // Cambia cada segundo
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setColor(getRandomColor());
+      }, 300); // Cambia cada segundo
 
-//     return () => clearInterval(interval); // Limpieza del intervalo
-//   }, []);
+      return () => clearInterval(interval); // Limpieza del intervalo
+    }, []);
 
   return (
-    <Pressable onPress={() => console.log("Chat presionado")}>
+    <Pressable onPress={() => router.push({
+      pathname: "/chat/group",
+    })}>
       <Text
         style={{
           color,
           fontSize: 24,
+          paddingHorizontal: 20,
           //textDecorationLine: "underline",
         }}
       >
